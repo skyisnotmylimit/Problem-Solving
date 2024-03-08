c9af748c40b60d2de39de5f7b70606f779ef35bd
ShubhamGupta9453 <shubhamgupta9454666551@gmail.com>
2024-03-08 23:05:02 +0530

    Q-5 some bugs encountered

---
+++ Searching-1/Students_having_heights_atleast_x.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+//Few bugs...passing 2/3 testcases....will look into it next day.
+class Students_having_heights_atleast_x {
+
+    public static int binarySearch(int[] arr,int target,int low,int high) {
+        if (low>=high) return low;
+        int mid = low + (high - low)/2;
+        if (arr[mid]>=target)
+        return binarySearch(arr,target,low,mid);
+        else
+        return binarySearch(arr,target,mid+1,high);
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int Q = in.nextInt();
+        int a[] = new int[n];
+        int ans[] = new int[Q];
+        for(int i=0;i<n;i++)
+        a[i] = in.nextInt();
+        Arrays.sort(a);
+        for(int i=0;i<Q;i++) {
+            int temp = in.nextInt();
+            int indx = binarySearch(a,temp,0,n-1);
+            ans[i] = (n-indx);
+        }
+        for(int i : ans)
+        System.out.println(i);
+    }
+}
